c649031fe1c3065ea8a6cec240a11f249fada49c
fru2 <utsavfrutu@gmail.com>
2024-03-30 21:23:00 +0530

    updates 2

---
+++ app/components/dashboard/Search.js
@@ -0,0 +1,15 @@
+import { Autocomplete, TextField } from '@mui/material';
+
+export default function Search() {
+    return (
+        <Autocomplete
+            className="flex-1 flex-grow w-max bg-white"
+            id="free-solo-demo"
+            freeSolo
+            // options={top100Films.map((option) => option.title)}
+            options={['gg', 'abc']}
+            renderInput={(params) => <TextField {...params}/>}
+            
+        />
+    )
+}
