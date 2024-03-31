73d6567b50d7150c94817af473865bd156145825
Varun3815 <vaarunkot@gmail.com>
2024-03-31 15:16:16 +0530

    adding author api

--- app/api/yearCount/route.js
+++ app/api/yearCount/route.js
@@ -2,21 +2,20 @@ import { NextResponse } from "next/server";
 import { dbConnect } from "../../../db";
 import ResearchPaper from "@/models/ResearchPaper";
 
+export const GET = async (req) => {
+  try {
+    await dbConnect();
+    const yearsCount = await ResearchPaper.aggregate([
+      { $group: { _id: "$Year", count: { $sum: 1 } } },
+      { $sort: { _id: 1 } }, // Optional: Sort by year in ascending order
+    ]);
 
-
-  export const GET = async (req) => {
-    try {
-      await dbConnect();
-      const yearsCount = await ResearchPaper.aggregate([
-        { $group: { _id: '$Year', count: { $sum: 1 } } },
-        { $sort: { _id: 1 } } // Optional: Sort by year in ascending order
-      ]);
-      
-      return new NextResponse(JSON.stringify({ result: yearsCount }), { status: 200 });
-    } catch (error) {
-      return new NextResponse("Error in fetching data: " + error, {
-        status: 500,
-      });
-    }
-  };
-  
+    return new NextResponse(JSON.stringify({ result: yearsCount }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new NextResponse("Error in fetching data: " + error, {
+      status: 500,
+    });
+  }
+};
