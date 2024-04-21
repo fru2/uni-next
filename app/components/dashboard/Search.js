94c01825f8674567872518827262d9c10c072cf4
raghav-sethi <76265403+raghav-sethi@users.noreply.github.com>
2024-04-21 15:41:43 +0530

    search redux implemented

--- app/components/dashboard/Search.js
+++ app/components/dashboard/Search.js
@@ -2,20 +2,21 @@
 
 import { Autocomplete, TextField } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
-// import { setSearch } from '../../store/reducers/filterReducer';
+import { setSearch } from '../../store/reducers/filterReducer';
 
 export default function Search() {
-//   const searchInput = useSelector((state) => state.filter.search);
-//   console.log(searchInput);
+    const dispatch = useDispatch();
+    const searchInput = useSelector((state) => state.filter.search);
+    // console.log(searchInput);
 
-  return (
-    <Autocomplete
-      className="flex-1 flex-grow w-max bg-white border-red"
-      id="free-solo-demo"
-      freeSolo
-      // options={top100Films.map((option) => option.title)}
-      options={['gg', 'abc']}
-      renderInput={(params) => <TextField placeholder="Search" {...params} />}
-    />
-  );
+    return (
+        <Autocomplete
+        className="flex-1 flex-grow w-max bg-white border-red"
+        id="free-solo-demo"
+        freeSolo
+        // options={top100Films.map((option) => option.title)}
+        options={['gg', 'abc']}
+        renderInput={(params) => <TextField value={searchInput} onChange={(e) => dispatch(setSearch(e.target.value))} placeholder="Search" {...params} />}
+        />
+    );
 }
